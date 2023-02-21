8f26b55639cc20f1dc8f3dbfa292df98f84d2c36
HANUMAT-SHARAN <deotadayal27@gmail.com>
2023-02-21 21:33:33 +0530

    product cred and queries of multiple type like lte gte page limit sort order category search added done

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,22 @@ const app = express();
 const cors = require("cors");
 const { connection } = require("./Configs/dataBase");
 const { UserRouter } = require("./Routes/UserRoute");
+const { ProductModel } = require("./Models/ProductModel");
+const { ProductsRouter } = require("./Routes/ProductsRoute");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/",UserRouter)
+app.use("/",ProductsRouter)
 
 app.listen(8080, async () => {
   console.log("Server Is Started At 8080");
   try {
     await connection;
     console.log("Server is Connected To DATABASE");
+// ProductModel.insertMany()
+//      console.log("updated")
   } catch (error) {
     console.log(error);
   }
