90b3138aa9e4b813c8f17348a2dbfae923885b03
HANUMAT-SHARAN <deotadayal27@gmail.com>
2023-02-26 03:16:26 +0530

    Single Page done with response and redux and add to cart Fuctionality

--- frontend/src/AllRoutes/Routes.js
+++ frontend/src/AllRoutes/Routes.js
@@ -1,22 +1,27 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import AdminPage from '../pages/AdminPage'
-import AdminPage1 from '../pages/AdminPage1'
-import AdminPage2 from '../pages/Adminpage2'
-import AdminPage3 from '../pages/AdminPage3'
-import CartPage from '../Pages/CartPage'
-import HomePage from '../pages/HomePage'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AdminPage from "../pages/AdminPage";
+import AdminPage1 from "../pages/AdminPage1";
+import AdminPage2 from "../pages/Adminpage2";
+import AdminPage3 from "../pages/AdminPage3";
+import CartPage from "../Pages/CartPage";
+import HomePage from "../pages/HomePage";
+import SingleProductPage from "../Pages/SingleProductPage";
 // import AdminLandingPage from '../pages/Check'
 const AllRoutes = () => {
   return (
     <div>
-      <Routes >
-<Route path="/" element={<HomePage/>} />
-<Route path='/admin' element={<AdminPage/>}></Route>
-<Route path='/admin/addtheproduct' element={<AdminPage1/>}></Route>
-<Route path='/admin/checktheusers' element={<AdminPage2/>}></Route>
-<Route path='/admin/seeallthecollections' element={<AdminPage3/>}></Route>
-<Route path='/cart' element={<CartPage/>}></Route>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/admin" element={<AdminPage />}></Route>
+        <Route path="/admin/addtheproduct" element={<AdminPage1 />}></Route>
+        <Route path="/admin/checktheusers" element={<AdminPage2 />}></Route>
+        <Route
+          path="/admin/seeallthecollections"
+          element={<AdminPage3 />}
+        ></Route>
+        <Route path="/cart" element={<CartPage />}></Route>
+        <Route path="/product/:id" element={<SingleProductPage/>} />
       </Routes>
     </div>
   );
