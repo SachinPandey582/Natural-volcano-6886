a3480b154a73f54423fd52770d5e40702e9b1ef5
HANUMAT-SHARAN <deotadayal27@gmail.com>
2023-02-26 19:18:13 +0530

    Private Route done

--- frontend/src/AllRoutes/PrivateRoute.jsx
+++ frontend/src/AllRoutes/PrivateRoute.jsx
@@ -1,32 +1,30 @@
 import React from 'react'
 import jwt_decode from "jwt-decode";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
 
-    const nav = useNavigate()
+   
 
     const token = localStorage.getItem("token")
 
    
-    if (token!=="1234") {
-        let decode=jwt_decode(token)
-        if(decode.user.role==="admin"){
-           return nav("/admin")
-        }else{
-          return nav("/")
-        }
+    let decode=jwt_decode(token)
+    if(decode.user.role==="user"){
+       return <Navigate to="/" />
     }
+
+   return <>
+   {children}
+   </>
+    
        
-   
+    
 
 
 
-    return (
-        <div>
-            {/* <Admin Route {children}*/}
-        </div>
-    )
+    
+    
 }
 
 
--- frontend/src/AllRoutes/Routes.js
+++ frontend/src/AllRoutes/Routes.js
@@ -8,18 +8,19 @@ import Beauty from "../Pages/BeautyPage";
 import CartPage from "../Pages/CartPage";
 import HomePage from "../Pages/HomePage";
 import SingleProductPage from "../Pages/SingleProductPage";
+import PrivateRoute from "./PrivateRoute";
 // import AdminLandingPage from '../pages/Check'
 const AllRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/admin" element={<AdminPage />}></Route>
-        <Route path="/admin/addtheproduct" element={<AdminPage1 />}></Route>
-        <Route path="/admin/checktheusers" element={<AdminPage2 />}></Route>
+        <Route path="/admin" element={<PrivateRoute><AdminPage/></PrivateRoute>}></Route>
+        <Route path="/admin/addtheproduct" element={<PrivateRoute><AdminPage1/></PrivateRoute>}></Route>
+        <Route path="/admin/checktheusers" element={<PrivateRoute><AdminPage2/></PrivateRoute>}></Route>
         <Route
           path="/admin/seeallthecollections"
-          element={<AdminPage3 />}
+          element={<PrivateRoute><AdminPage3/></PrivateRoute>}
         ></Route>
         
         <Route path="/products" element={<Beauty />}></Route>
