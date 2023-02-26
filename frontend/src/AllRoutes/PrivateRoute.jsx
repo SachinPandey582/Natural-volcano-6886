24cb4d019e3e47799a7eb8f5c7a024f093b663bf
HANUMAT-SHARAN <deotadayal27@gmail.com>
2023-02-26 19:47:58 +0530

    Private Route done

--- frontend/src/AllRoutes/PrivateRoute.jsx
+++ frontend/src/AllRoutes/PrivateRoute.jsx
@@ -4,27 +4,27 @@ import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
 
-   
+
 
     const token = localStorage.getItem("token")
 
-   
-    let decode=jwt_decode(token)
-    if(decode.user.role==="user"){
-       return <Navigate to="/" />
+
+    let decode = jwt_decode(token)
+    if (decode.user.role === "user") {
+        return <Navigate to="/" />
     }
 
-   return <>
-   {children}
-   </>
-    
-       
-    
+    return <>
+        {children}
+    </>
 
 
 
-    
-    
+
+
+
+
+
 }
 
 
